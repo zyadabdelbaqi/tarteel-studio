d7bb9ca700bb8a4175a42360a5607c762cb2c34f
zyadabdelbaqi <zyadzoiruwk@gmail.com>
2026-07-13 18:21:23 +0300

    Add files via upload

---
+++ test-font.js
@@ -0,0 +1,3 @@
+const { Worker } = require('worker_threads');
+// Just checking if node has issues with string concatenation
+console.log('bold ' + (35 * 0.66666) + 'px "Scheherazade New", "Noto Sans Arabic", sans-serif');
